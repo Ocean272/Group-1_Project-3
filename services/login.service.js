b13471ecfdfd33326d62d2c2f533715ac5a32065
Ocean272 <ocean272@hotmail.com>
2026-04-03 13:25:04 +0800

    delete types/bcrypt from package.json

--- services/login.service.js
+++ services/login.service.js
@@ -1,8 +1,12 @@
 const { User } = require("../models/");
-const bcrypt = require("bcryptjs");
+const bcrypt= require ("bcryptjs");
 
 module.exports = {
+
   create: async (username, email, password) => {
+
+    const salt = bcrypt.genSaltSync(10);
+
     //The result object is where we will put the result to be sent to th client.
     let result = {
       message: null,
@@ -13,7 +17,7 @@ module.exports = {
     const newUser = await User.create({
       username: username,
       email: email,
-      password: bcrypt.hashSync(password, 10),
+      password: bcrypt.hashSync(password, salt),
     });
 
     await newUser.save(); // update the User
